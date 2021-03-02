b9eb4378e7e8e0fb61dbc5701b017af1a62bf17d
Peter Leiva <pleiva@id.uff.br>
2021-03-02 20:51:15 -0300

    Fix import path from specs

--- src/lib/grouper/__tests__/aggregator.spec.js
+++ src/lib/grouper/__tests__/aggregator.spec.js
@@ -1,89 +1,89 @@
-import Aggregator from '../lib/grouper/name-date.js';
+import Aggregator from "../name-date.js";
 
-describe('NameDateAggregator', () => {
+describe("NameDateAggregator", () => {
   let strategy;
 
   afterEach(() => (strategy = null));
 
-  describe('No name giving', () => {
-    it('Throws no given name error', () => {
-      expect(new Aggregator()).toThrowError('Name must be a string');
+  describe("No name giving", () => {
+    it("Throws no given name error", () => {
+      expect(new Aggregator()).toThrowError("Name must be a string");
     });
   });
 
-  describe('With given name', () => {
-    describe('.test', () => {
-      describe('Returns false when', () => {
-        it('Has only numbers', () => {
-          strategy = new Aggregator('123482');
+  describe("With given name", () => {
+    describe(".test", () => {
+      describe("Returns false when", () => {
+        it("Has only numbers", () => {
+          strategy = new Aggregator("123482");
           expect(strategy.filter()).toBe(false);
         });
 
-        it('Has only non-space caracteres', () => {
-          strategy = new Aggregator('abc');
+        it("Has only non-space caracteres", () => {
+          strategy = new Aggregator("abc");
           expect(strategy.filter()).toBe(false);
         });
 
-        it('Miss last 2 char number', () => {
-          strategy = new Aggregator('name - 2020-02-01-23-20');
+        it("Miss last 2 char number", () => {
+          strategy = new Aggregator("name - 2020-02-01-23-20");
           expect(strategy.filter()).toBe(false);
         });
 
-        it('Has only the date part', () => {
-          strategy = new Aggregator('2020-02-01-23-20-50');
+        it("Has only the date part", () => {
+          strategy = new Aggregator("2020-02-01-23-20-50");
           expect(strategy.filter()).toBe(false);
         });
 
-        it('Has only the name part', () => {
-          strategy = new Aggregator('name - ');
+        it("Has only the name part", () => {
+          strategy = new Aggregator("name - ");
           expect(strategy.filter()).toBe(false);
         });
 
-        it('No space between name and date', () => {
-          strategy = new Aggregator('name-2020-02-01 23-20-50');
+        it("No space between name and date", () => {
+          strategy = new Aggregator("name-2020-02-01 23-20-50");
           expect(strategy.filter()).toBe(false);
         });
 
-        it('Date is all separated by dash', () => {
-          strategy = new Aggregator('name - 2020-02-01-23-20-50');
+        it("Date is all separated by dash", () => {
+          strategy = new Aggregator("name - 2020-02-01-23-20-50");
           expect(strategy.filter()).toBe(false);
         });
       });
 
-      describe('Returns true when', () => {
-        const name = 'name - 2020-02-01 23-20-50';
+      describe("Returns true when", () => {
+        const name = "name - 2020-02-01 23-20-50";
 
-        it('Have exact format <name> - <date>', () => {
+        it("Have exact format <name> - <date>", () => {
           strategy = new Aggregator(name);
           expect(strategy.filter()).toBe(true);
         });
 
-        it('Has a extension', () => {
-          strategy = new Aggregator(name + '.txt');
+        it("Has a extension", () => {
+          strategy = new Aggregator(name + ".txt");
           expect(strategy.filter()).toBe(true);
         });
 
-        it('Name all in caps', () => {
+        it("Name all in caps", () => {
           strategy = new Aggregator(name.toUpperCase());
           expect(strategy.filter()).toBe(true);
         });
 
-        it('Has special caracteres in name', () => {
-          strategy = new Aggregator('é' + name);
+        it("Has special caracteres in name", () => {
+          strategy = new Aggregator("é" + name);
           expect(strategy.filter()).toBe(true);
         });
       });
     });
 
-    describe('.naming', () => {
-      it('Get null when no match', () => {
-        const strategy = new Aggregator('name - ');
+    describe(".naming", () => {
+      it("Get null when no match", () => {
+        const strategy = new Aggregator("name - ");
         expect(strategy.naming()).toBeNull();
       });
 
-      it('Get group as <name> from <name> - <date>', () => {
-        const strategy = new Aggregator('filename - 2020-02-01 23-20-50');
-        expect(strategy.naming()).toBe('filename');
+      it("Get group as <name> from <name> - <date>", () => {
+        const strategy = new Aggregator("filename - 2020-02-01 23-20-50");
+        expect(strategy.naming()).toBe("filename");
       });
     });
   });
--- src/lib/grouper/__tests__/alphabetical.spec.js
+++ src/lib/grouper/__tests__/alphabetical.spec.js
@@ -1,33 +1,33 @@
-import Aggregator from '../lib/grouper/alphabetical.js';
+import Aggregator from "../alphabetical.js";
 
-describe('Alphabetical', () => {
-  describe('.naming', () => {
-    it('Returns # when begins with number', () => {
-      const strategy = new Aggregator('10 arquivo');
-      expect(strategy.naming()).toBe('#');
+describe("Alphabetical", () => {
+  describe(".naming", () => {
+    it("Returns # when begins with number", () => {
+      const strategy = new Aggregator("10 arquivo");
+      expect(strategy.naming()).toBe("#");
     });
 
-    it('Get the first letter of file', () => {
-      const strategy = new Aggregator('arquivo');
-      expect(strategy.naming()).toBe('A');
+    it("Get the first letter of file", () => {
+      const strategy = new Aggregator("arquivo");
+      expect(strategy.naming()).toBe("A");
     });
 
-    describe('Special caracteres', () => {
-      it('especial when begin with space', () => {
-        const strategy = new Aggregator(' arquivo');
-        expect(strategy.naming()).toBe('especial');
+    describe("Special caracteres", () => {
+      it("especial when begin with space", () => {
+        const strategy = new Aggregator(" arquivo");
+        expect(strategy.naming()).toBe("especial");
       });
 
-      it('especial when begin with underscore', () => {
-        const strategy = new Aggregator('_arquivo');
-        expect(strategy.naming()).toBe('especial');
+      it("especial when begin with underscore", () => {
+        const strategy = new Aggregator("_arquivo");
+        expect(strategy.naming()).toBe("especial");
       });
     });
   });
 
-  describe('.filter', () => {
-    it('Always returns true', () => {
-      expect(new Aggregator('whaetever').filter()).toBe(true);
+  describe(".filter", () => {
+    it("Always returns true", () => {
+      expect(new Aggregator("whaetever").filter()).toBe(true);
     });
   });
 });
