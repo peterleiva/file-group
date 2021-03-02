b62d79dafdcf0157dd39f80651835b279a3eea39
Peter Leiva <pleiva@id.uff.br>
2021-03-02 17:54:10 -0300

    Use __dirname witouth  type "module" from native node modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /**
  * @file Group files, other than directories, using certain criteras. Those
  * criateras defines two algorythm, one checking if a file is ok to be grouped
@@ -7,32 +5,28 @@
  */
 // TODO: passar dirent para algoritmo de agregação para possa filtrar por tipo
 // TODO: Agrupar alfabeticamente usando subpastas ou arquivos
-
-import { fileURLToPath } from 'url';
-import fs from 'fs/promises';
-import path from 'path';
-import readline from 'readline';
-import program from './lib/commander.js';
-import organizer from './lib/organizer.js';
-import mapper from './lib/mapper.js';
-import stats from './lib/stats.js';
+import fs from "fs/promises";
+import path from "path";
+import readline from "readline";
+import program from "./lib/commander.js";
+import organizer from "./lib/organizer.js";
+import mapper from "./lib/mapper.js";
+import stats from "./lib/stats.js";
 
 const ACCEPTANCE_PATTERN = /^(y|yes)$/i;
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /**
  * Main point from the application
- * TODO: melhorra errors ao abrir diretório
+ * TODO: melhor errors ao abrir diretório
  * @param {string} base
  */
 async function main(base, aggregator) {
   let Aggregator;
   try {
-    const module = await import(
-      path.resolve(__dirname, 'lib', 'grouper', aggregator + '.js')
-    );
-    Aggregator = module.default;
+    ({ default: Aggregator } = await import(
+      path.resolve(__dirname, "lib", "grouper", aggregator)
+    ));
   } catch (error) {
-    console.error('Failed to load aggregator algorithm');
+    console.error("Failed to load aggregator algorithm");
     throw error;
   }
 
@@ -41,7 +35,7 @@ async function main(base, aggregator) {
   try {
     directory = await fs.opendir(base);
   } catch (error) {
-    console.error('Failed to open directory: ', path.resolve(base));
+    console.error("Failed to open directory: ", path.resolve(base));
     throw error;
   }
 
@@ -55,9 +49,9 @@ async function main(base, aggregator) {
     output: process.stdout,
   });
 
-  rl.question('Are you sure (y/N)?: ', async (answer) => {
+  rl.question("Are you sure (y/N)?: ", async (answer) => {
     if (!ACCEPTANCE_PATTERN.exec(answer)) {
-      console.info('You answer is NO');
+      console.info("You answer is NO");
       return rl.close();
     }
 
