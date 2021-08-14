95f30e2ebfea5c320bf27ae40128457d34739e86
Peter <85527528+pherval@users.noreply.github.com>
2021-08-14 07:17:02 -0300

    remove unecessary webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const { merge } = require("webpack-merge");
 const nodeExternals = require("webpack-node-externals");
 const webpack = require("webpack");
 const ESLintPlugin = require("eslint-webpack-plugin");
@@ -10,22 +9,21 @@ const ESLintPlugin = require("eslint-webpack-plugin");
  * @param {"production" | "development"} env.mode webpack mode
  * @return {import('webpack').Configuration}
  */
-module.exports = ({ mode }) =>
-  merge({
-    target: "node",
-    mode,
-    externals: [nodeExternals()],
+module.exports = ({ mode }) => ({
+  target: "node",
+  mode,
+  externals: [nodeExternals()],
 
-    plugins: [
-      new ESLintPlugin(),
-      new webpack.BannerPlugin({
-        banner: "#!/usr/bin/env node",
-        entryOnly: true,
-        raw: true,
-      }),
-    ],
-    output: {
-      filename: "index.bundled.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-  });
+  plugins: [
+    new ESLintPlugin(),
+    new webpack.BannerPlugin({
+      banner: "#!/usr/bin/env node",
+      entryOnly: true,
+      raw: true,
+    }),
+  ],
+  output: {
+    filename: "index.bundled.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+});
