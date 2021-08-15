bc1558e4fd93fef968cb29cd33512f0bab528885
Peter <85527528+pherval@users.noreply.github.com>
2021-08-15 02:42:40 -0300

    re-export lib with its index.js

---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+export { default as mapper } from "./mapper";
+export { default as commander } from "./commander";
+export { default as organizer } from "./organizer";
+export { default as stats } from "./stats";
